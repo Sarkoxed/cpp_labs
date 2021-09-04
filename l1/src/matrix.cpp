c1a5c763e34aa11c0cd37dcb8d711abd8bfaa710
Sarkoxed <ss2003@yandex.ru>
2021-09-04 23:12:13 +0300

    rewrote the whole programm, added classes csw,line, el defined all the functions and wrote them for line started writing functions for csw

--- l1/src/matrix.cpp
+++ l1/src/matrix.cpp
@@ -1,9 +1,14 @@
-#include "matrix.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+
+#ifndef MAIN_MATRIX
+#define MAIN_MATRIX
 
 template <typename T>
-void get_el(T &x){
+void get_el(int &T){
     try{
-        int n;
+        T n;
         std::cin >> n;
         x = n;
     }
@@ -12,37 +17,97 @@ void get_el(T &x){
     }
 }
 
-template <typename T>
-std::ostream&operator<<(std::ostream&out, const std::vector<std::vector<T>> &x){
-    for(auto p1: x){
-       for(auto p2: p1){
-           std::cout << p2 << " ";
-       }
-       std::cout << std::endl;
-    }
-    return out;
-}
 
 template <typename T>
-void init_matr(std::vector<std::vector<T>> &x){
-    int n, m;
-    T el;
-    std::cout << "insert the amount of lines: ";
-    get_el(n);
-    std::cout << std::endl; 
-    x.resize(n);
-    for(int i = 0; i < n; i++){
-        std::cout << "insert the amount of elements in line "<< i << ": ";
-        get_el(m);
-        std::cout << std::endl;
-        x[i].resize(m);
-            for(int j = 0; j < m; j++){
-            std::cout << "element " << j << ": ";
-            get_el(el);
-            std::cout << std::endl;
-            x[i][j] = el;
+class el{
+    public:
+        el(int ind = 0, T val = 0):ind(ind), val(val){}
+        int ind(){ return _ind; }
+        T val(){ return _val;}
+    protected:
+        int _ind;
+        T _val;
+};
+
+
+class line : el{
+    public:
+        line(int total = 0){
+            this->am = 0;
+            std::vector<el> this->els;
+            this->matr_size = total;
+        }
+        
+        void size(){ return els.length() };
+        void add(el::el &x);
+        void del(int ind);
+        int get(int ind);
+        friend std::ostream&operator<<(std::ostream&out,const line &x);    
+    protected:
+        std::vector<el> els;
+        int matr_size;
+};
+
+
+void line::add(el::el &x){
+    this->els.resize(this->size()+1); 
+    this->els.push_back(x);
+}
+
+void line::del(int ind){
+    int g = this->get(ind);
+    if(g > 0){
+        this->els.erase(this->els.begin() + g);
+    }
+}
+
+int line::get(int ind){
+    int beg = 0, end = this->size() - 1;
+    while(beg <= end ){
+        int m = (end + beg) / 2;
+        if(this->els[m].ind() < ind){
+            beg = m + 1;
+        }
+        else if(this->els[m].ind() > ind){
+            end = m - 1;
+        }
+        else{
+            return m;
         }
     }
+    return -1;
+}
+
+std::ostream&operator<<(std::ostream&out, const line::line &x){
+    auto p this->els.begin();
+    for(int i = 0; i < this->matr_size; i++){
+        if(*p.ind() == i){
+            std::cout << *(p++).val() << " ";
+        }
+        else{
+            std::cout << 0 << " ";
+        }
+    } 
 }
 
 
+template <typename T>
+class csw : line{
+    public:
+        csw(int am = 0){
+            std::vector<line> this->lines(am); 
+        }
+        int size(){ return lines.length();}
+        void add(int ind_x, int ind_y, T v){
+            el::el x = el(ind_y, v);
+            
+        } 
+        void del(int ind_x);
+        void del(int ind_x, int ind_y);
+        friend std::ostream&operator<<(std::ostream&out,const csw &x);    
+
+    private:
+        std::vector<line> lines;
+};
+
+#endif
