f4ec33186f4a486dc96150ac6684425235a95178
Sarkoxed <ss2003@yandex.ru>
2021-09-05 04:27:18 +0300

    task is done

--- l1/src/main.cpp
+++ l1/src/main.cpp
@@ -1,8 +1,10 @@
 #include "matrix.hpp"
 #include <iostream>
 int main(){
-    std::vector<std::vector<int>> matr;
-    init_matr(matr); 
+    csw<int> matr;
+    matr = init_matr();
+    matr.debug_print();
     std::cout << matr;
+    std::vector<int> ans = create_vec(matr, crit);
+    std::cout << "Sum of palindromes of each line is: " << ans;
 }
-
