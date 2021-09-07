52cb197dbcab44681dad4000f0a565d6a93a5331
Sarkoxed <ss2003@yandex.ru>
2021-09-07 21:36:40 +0300

    now you are able to choose

--- l1/src/main.cpp
+++ l1/src/main.cpp
@@ -1,13 +1,28 @@
 #include "matrix.hpp"
 #include <iostream>
 
-int main(){
-    csw<int> matr;
-    matr = init_matr();
+    int main(){
+        csw<int> matr;
+        matr = init_matr();
+        matr.debug_print();
+        std::cout << matr;
+        std::cout << "Palindromes or Double digits?(0/1): "; 
+        int choice;
+        std::string s;
+        std::vector<int> ans; 
 
-    matr.debug_print();
-    std::cout << matr;
-
-    std::vector<int> ans = create_vec(matr, crit);
-    std::cout << "Sum of palindromes of each line is: " << ans;
-}
+        get_el(choice);
+        switch (choice){
+            case 0:
+                ans = create_vec(matr, crit);
+                s = "palindromes";
+                break;
+            case 1:
+                ans = create_vec(matr, crit1);
+                s = "double-digits";
+                break;
+            default:
+                exit(1);
+        }
+        std::cout << "Sum of " << s << " of each line is: " << ans;
+    }
