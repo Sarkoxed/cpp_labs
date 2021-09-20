462de68837d2844d5782aa906f0fb2a45e9f3bc2
Sarkoxed <ss2003@yandex.ru>
2021-09-21 00:12:44 +0300

    done the lib, tests are coming

---
+++ l2/src/lib.cpp
@@ -0,0 +1,24 @@
+#include "lib.hpp"
+
+hypotypes hypocycloid:: get_type(){
+    double sub = this->_r - this->_d;
+    if(sub == 0.0){
+        return hypotypes::normal;
+    }
+    else if(sub < 0.0){
+        return hypotypes::extended;
+    }
+    else{
+        return hypotypes::shortened;
+    }
+}
+
+double hypocycloid:: sectorial_area(){
+    double a = M_1_PI*2.0;
+    return (this->_k + 1.0) / 2.0 * (a * (this->_r * this->_r * (this->_k + 1.0) + this->_d * this->_d) - this->_d * this->_r * (this->_k + 2.0) / this->_k * sin(this->_k * a));
+}
+
+double hypocycloid:: curvative_radius(double phi){
+    return (this->_k + 1.0) * pow(this->_r * this->_r + this->_d * this->_d - 2.0 * this->_r * this->_d * cos(this->_k * phi), 1.5) / fabs(pow(this->_r, 2.0) + pow(this->_d, 2.0) * (this->_k + 1.0) - this->_r * this->_d * (this->_k + 2.0)*cos(this->_k * phi));
+}
+
--- l2/src/lib.hpp
+++ l2/src/lib.hpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <cmath>
 
+#ifndef LIM_CYCLE
+#define LIM_CYCLE
+
+enum class hypotypes : short{normal, extended, shortened}; 
+
+std::ostream& operator<<(std::ostream &out, hypotypes x){
+    switch (x){
+        case hypotypes::normal: std::cout << "normal" << std::endl;
+        case hypotypes::extended: std::cout << "extended" << std::endl;
+        case hypotypes::shortened: std::cout << "shortened" << std::endl;
+    }
+   return out; 
+}
+
 template <typename T>
-class point{
+class pair{
     public:
-        point():_first(0.0), _second(0.0){}
-        point(T f, T s):_first(f), _second(s){}
+        pair(T f, T s):_first(f), _second(s){}
         T x(){return this->_first;}
         T y(){return this->_second;}
     private: 
@@ -13,24 +26,61 @@ class point{
         T _second;
 };
 
-class hypcy : point<double>{
+class hypocycloid{
     public:
-        hypcy():_r(0.0), _k(1.0){}
-        hypcy(double r, double k):_r(r), _k(k){}
-        
-        double get_x(double phi){ return this->_r * (this->_k - 1) * cos(phi) + this->_r * cos((this->_k - 1) * phi);}
-        double get_y(double phi){ return this->_r * (this->_k - 1) * sin(phi) - this->_r * sin((this->_k - 1) * phi);}
-
-
-        point<double> get_point(double phi){
-            return point<double>(get_x(phi), get_y(phi));
+        hypocycloid():_r(1.0), _k(1.0), _d(1.0){}
+        hypocycloid(double r, double k):_r(r), _k(k), _d(r){
+            if(r <= 0.0 || k <= 0.0){
+                throw "no negative allowed";
+            }
+        }
+        hypocycloid(double r, double k, double d):_r(r), _k(k), _d(d){
+            if(r <= 0.0 || k <= 0.0 || d <= 0.0){
+                throw "no negative allowed";
+            }
         }
         
-        point<double> get_rs(){ return point<double>(this->_r, this->_r * this->_k);}
-            
-       
+        void set_r(double r){ 
+            if(r <= 0.0){
+                throw "no negative allowed";
+            }
+            this->_r = r;
+        }
+        void set_k(double k){ 
+            if(k <= 0.0){
+                throw "no negative allowed";
+            }
+            this->_k = k;
+        }
+        void set_d(double d){ 
+            if(d <= 0.0){
+                throw "no negative allowed";
+            }
+            this->_d = d;
+        }
+
+        double get_r(double r){return this->_r = r;}
+        double get_k(double k){return this->_k = k;}
+        double get_d(double d){return this->_d = d;}
+
+        double get_x(double phi){ return this->_r * (this->_k - 1) * cos(phi) + this->_d * cos((this->_k - 1) * phi);}
+        double get_y(double phi){ return this->_r * (this->_k - 1) * sin(phi) - this->_d * sin((this->_k - 1) * phi);}
         
+        hypotypes get_type();
+        pair<double> get_point(double phi){
+            return pair<double>(get_x(phi), get_y(phi));
+        }
+
+        double sectorial_area(); 
+        
+        double curvative_radius(double phi);
+        pair<double> get_rs(){ return pair<double>(this->_r, this->_r * this->_k);}
+
     private:
         double _r;
         double _k;
+        double _d;
 };
+
+
+#endif 
