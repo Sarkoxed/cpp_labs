adcc55ccdaf026ad56779eeb6b4d4fd482d0a102
Sarkoxed <ss2003@yandex.ru>
2021-09-24 14:11:05 +0300

    added all the tests created library linked it and stuff like this also changet get_area function

---
+++ l2/gtests/tesrc/lib.hpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cmath>
+
+#ifndef LIM_CYCLE
+#define LIM_CYCLE
+namespace curve{
+    enum class hypotypes : short{normal, extended, shortened}; 
+    
+
+    template <typename T, typename H>
+    class pair{
+        public:
+            pair():_first(0.0), _second(0.0){}
+            pair(T f, H s):_first(f), _second(s){}
+            T x() const {return this->_first;}
+            H y() const {return this->_second;}
+            
+            pair<T, H>& operator=(const pair<T, H> &e){
+                this->_first = e._first;
+                this->_second = e._second;
+                return *this;
+            }
+            pair(const pair<T,H> &x){
+                this->_first = x._first;
+                this->_second = x._second;
+            }
+
+            friend std::ostream& operator<<(std::ostream &out, const pair<T, H> &x){
+                std::cout << "(" << x._first << ", " << x._second << ") " << std::endl;
+                return out;
+            }
+
+            friend std::fstream& operator<<(std::fstream &out, const pair<T,H> &x){
+                std::cout << "(" << x._first << ", " << x._second << ") " << std::endl;
+                return out;
+
+            }
+
+
+        private: 
+            T _first;
+            H _second;
+    };
+    
+
+
+
+
+    class hypocycloid{
+        public:
+            hypocycloid():_r(1.0), _k(1.0), _d(1.0){}
+
+            hypocycloid(double r, double k):_r(r), _k(k), _d(r){
+                if(r <= 0.0 || k <= 0.0){
+                    throw "no z-negative allowed";
+                }
+            }
+            hypocycloid(double r, double k, double d):_r(r), _k(k), _d(d){
+                if(r <= 0.0 || k <= 0.0 || d <= 0.0){
+                    throw "no z-negative allowed";
+                }
+            }
+
+            hypocycloid(const hypocycloid &x):_r(x._r), _k(x._k), _d(x._d){}
+
+            hypocycloid& operator=(const hypocycloid &x);                        
+            void set_r(double r);
+            void set_k(double k);
+            void set_d(double d);            
+
+            double get_r() const {return this->_r;}
+            double get_k() const {return this->_k;}
+            double get_d() const {return this->_d;}
+    
+            hypotypes get_type() const;
+
+            pair<double, double> get_point(double phi);    
+            double sectorial_area() const; 
+            double curvative_radius(double phi) const;
+            
+            pair<double, double> get_rs();
+        private:
+
+            double get_x(double phi);
+            double get_y(double phi);
+
+            double _r;
+            double _k;
+            double _d;
+    };
+
+}
+
+    std::ostream& operator<<(std::ostream &out, curve::hypotypes x);
+#endif 
---
+++ l2/gtests/tesrc/test.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include "lib.hpp"
+#include <cassert>
+
+//ASSERT_TRUE(condition);
+//ASSERT_FALSE(condition);
+//ASSERT_EQ(expected, actual); — =
+//ASSERT_NE(val1, val2); — !=
+//ASSERT_LT(val1, val2); — <
+//ASSERT_LE(val1, val2); — <=
+//ASSERT_GT(val1, val2); — >
+//ASSERT_GE(val1, val2); — >=
+//ASSERT_STREQ(expected_str, actual_str);
+//ASSERT_STRNE(str1, str2);
+//ASSERT_STRCASEEQ(expected_str, actual_str); — регистронезависимо
+//ASSERT_STRCASENE(str1, str2); — регистронезависимо
+//ASSERT_THROW(statement, exception_type);
+//ASSERT_ANY_THROW(statement);
+//ASSERT_NO_THROW(statement)
+//ASSERT_PREDN(pred, val1, val2, ..., valN); — N <= 5
+//ASSERT_PRED_FORMATN(pred_format, val1, val2, ..., valN
+//ASSERT_FLOAT_EQ(expected, actual); — неточное сравнение float
+//ASSERT_DOUBLE_EQ(expected, actual); — неточное сравнение double
+//ASSERT_NEAR(val1, val2, abs_error); — разница между val1 и val2 не превышает
+//SUCCEED();
+//FAIL();
+//ADD_FAILURE();
+//ADD_FAILURE_AT(«file_path», line_number);
+
+
+using namespace curve;
+
+TEST(curve, curve_point){
+    hypocycloid a;
+    hypocycloid b(2.0, 4.0);
+    hypocycloid c(2.0, 4.0, 3.2);
+    hypocycloid d(2, 4.2);
+    double x = a.get_point(32.0).x(), y = a.get_point(32.0).y();
+
+    EXPECT_DOUBLE_EQ(1.0, x);
+    EXPECT_DOUBLE_EQ(0.0, y);
+    EXPECT_DOUBLE_EQ(sqrt(3)*3.0, b.get_point(M_PI / 3.0).y());
+
+    ASSERT_THROW(hypocycloid(-1.0, 1.0, 1.0), char*);
+    ASSERT_THROW(hypocycloid(0, 2.0, 3.0), char*);
+    ASSERT_THROW(a.set_d(-1.0), char*); 
+    ASSERT_THROW(a.set_r(-1.0), char*);
+    ASSERT_THROW(a.set_k(-1.0), char*);
+    
+    EXPECT_DOUBLE_EQ(2.0 , a.get_rs().x()); 
+    EXPECT_DOUBLE_EQ(8.0,  a.get_rs().y());
+
+    EXPECT_DOUBLE_EQ(M_PI * 24, a.sectorial_area());
+
+}
+
+
+int main(){
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
