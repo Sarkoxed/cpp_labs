5b0218e13993adfab4e96d3438116043b5100e77
Sarkoxed <ss2003@yandex.ru>
2021-11-03 05:03:08 +0300

    getEl

---
+++ l3e/tools/toolib.hpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+#pragma once 
+
+template <typename T>
+void getEl(T& x,std::istream& in = std::cin, std::ostream& err = std::cerr){
+    bool valid;
+    do{
+        try{
+            valid = true;
+            in >> x;
+            if(in.fail()){
+                throw "bad input";
+            }
+        }
+        catch(const char * &e){
+            err << e;
+            if(in.bad() || in.eof()){
+                err << ", bye" << std::endl;
+                exit(0);
+            }
+            valid = false;
+            err << ", retry" << std::endl; 
+            in.clear();
+            in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }while(!valid);
+}
+
+template <typename T>
+std::ostream&operator<<(std::ostream& out,const std::vector<T> &x){
+    for(auto i: x){
+        out << i << " ";
+    }
+    out << std::endl;
+    return out;
+}
+
