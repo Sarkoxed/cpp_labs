379c02d28d3c47650e2385301bd0d1cbfe8cf7d3
Sarkoxed <ss2003@yandex.ru>
2021-12-17 06:00:39 +0300

    testing

--- l4/lol.cpp
+++ l4/lol.cpp
@@ -1,28 +1,14 @@
 // sarkoxed //
-
-#include <gtest/gtest.h>
-#include <cassert>
 #include <ostream>
-#include <map>
 
-#include "tools/toolib.hpp"
-#include "src/items/item.hpp"
-#include "src/items/medkit.hpp"
-#include "src/items/bandolier.hpp" 
-#include "src/items/weapon.hpp"
-#include "src/inventory/inventory.hpp"
-#include "src/character/character.hpp"
-#include "src/character/oper.hpp"
-#include "src/character/beasts.hpp"
-
+#include "src/level/level.hpp"
 int main(){
-    std::map<WeaponType, std::vector<unsigned int>> x = readweapon("config/weapons.conf");
-    std::pair<std::vector<std::string>, std::vector<std::vector<unsigned int>>> y = readchar("config/oper.conf");
+    srand(time(0));
+    wconfig wep = readweapon("config/weapons.conf");
+    opconfig op = readchar("config/oper.conf");
+    bconfig be = readbea("config/beasts.conf");
+    mapconfig ma = readmap("config/map.conf");
 
-    OpAgent a(y);
-    //std::cout << a.getCurWeight() << std::endl;
-    //std::cout << a.getMaxHealth() << std::endl;
-    std::cout << a.getName() << std::endl;
-    std::cout << a.getRelTime() << std::endl;
-    std::cout << a.getStrength() <<std::endl;
+    Level lvl(ma, wep, op, be, 3);
+    std::cout << lvl;
 }
